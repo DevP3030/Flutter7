8eec81bfc2cb2a9a4a78af578ff7c8e75b5941cc
DevP3030 <wadekarprati3@gmail.com>
2025-01-05 22:13:14 +0530

    Flutter 7

---
+++ android/app/src/main/kotlin/com/sos/flutter_7/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sos.flutter_7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp
+        (
+          home: Text("Sos : Be Brave To Make Difference"),
+        )
+  );
+}
